95ddb1a05f761b4ffcf20b9bd3d7b18680ff94aa
HsuMonLynn <scm.hsumonlynn@gmail.com>
2023-02-17 11:38:33 +0630

    add recommantation

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import DiscoverView from "../views/DiscoverView.vue"
-import Recommendations from "../views/Recommendations.vue"
-import WishListView from "../views/WishListView.vue"
-import SettingView from "../views/SettingView.vue"
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import DiscoverView from '../views/DiscoverView.vue';
+import RecommendationsView from '../views/RecommendationsView.vue';
+import WishListView from '../views/WishListView.vue';
+import SettingView from '../views/SettingView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/discover",
-      name: "discover",
-      component: DiscoverView
-    },
-    {
-      path: "/recommendations",
-      name: "recommendations",
-      component: Recommendations
-    },
-    {
-      path: "/wishlist",
-      name: "wishlist",
-      component: WishListView
-    },
-    {
-      path: "/setting",
-      name: "setting",
-      component: SettingView
-    }
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/discover',
+            name: 'discover',
+            component: DiscoverView
+        },
+        {
+            path: '/recommendations',
+            name: 'recommendations',
+            component: RecommendationsView
+        },
+        {
+            path: '/wishlist',
+            name: 'wishlist',
+            component: WishListView
+        },
+        {
+            path: '/setting',
+            name: 'setting',
+            component: SettingView
+        }
+    ]
 });
 
 export default router;
