d415dd4148ae65d0686074affc274c9cd2dc0ca7
AyeMyatMyatKhine19 <ayemyat.myatkhine@seattleconsultingmyanmar.com>
2023-02-24 15:00:15 +0630

    register

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-[url('../../images/register-wallpaper.jpg')] bg-cover bg-center bg-fixed h-screen">
+        <div class="bg-[#1C213E] text-[#fff] w-6/12 content-center">
+            <h1>Register Here!</h1>
+        </div>
+    </div>
+</template>
